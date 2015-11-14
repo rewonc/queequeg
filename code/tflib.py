93faa3542e7c1eccfcc6144465196f7c39f95257
Rewon Child <rewonc@gmail.com>
2015-11-14 12:23:39 -0800

    Add deconv op

--- code/tflib.py
+++ code/tflib.py
@@ -26,6 +26,61 @@ def _variable_summary(x):
     tf.histogram_summary(variable_name + '/values', x)
 
 
+def dropout(t, keep_prob):
+    """
+    Apply dropout to a tensor with the given probability
+
+    This will also reshape the output to fix a bug with dropout not keeping
+    shape.
+    """
+    dropped = tf.nn.dropout(t, keep_prob)
+    reshaped = tf.reshape(t, t.get_shape(), name=t.op.name + '/do')
+    return reshaped
+
+
+def deconv_bn_relu(t, kernel_size, stride, out_filters,
+                   scope, summarize=False):
+    '''
+    Apply a deconv operation (performs the reverse of a conv op)
+    '''
+
+    shape = t.get_shape()
+    assert len(shape) == 4, "Must be 4d input tensor"
+    batch_size = shape[0].value
+    w = shape[1].value
+    h = shape[2].value
+    in_filters = shape[3].value
+    kernel_shape = kernel_size + [out_filters, in_filters]
+    kernel_init = tf.random_normal_initializer(
+        0.0, 2.0 / (reduce(mul, kernel_shape, 1)))
+    stride_shape = [1] + stride + [1]
+    out_shape = [batch_size, w * stride[0], h * stride[0], out_filters]
+    # beta is the bias value to be added after batch norm
+    beta_init = tf.constant_initializer(0.0)
+    # gamma is the scalar value to be multiplied after batch norm
+    gamma_init = tf.constant_initializer(1.0)
+
+    with tf.variable_scope(scope) as scope:
+        conv_kernel = tf.get_variable(
+            "conv_kernel", kernel_shape, initializer=kernel_init)
+        conv = tf.nn.deconv2d(t, conv_kernel, out_shape, stride_shape,
+                              padding='SAME', name="conv_activations")
+        mean, var = tf.nn.moments(conv, axes=[0, 1, 2])
+        beta = tf.get_variable("beta", [out_filters], initializer=beta_init)
+        gamma = tf.get_variable("gamma", [out_filters], initializer=gamma_init)
+        bn = tf.nn.batch_norm_with_global_normalization(
+            conv, mean, var, beta, gamma, 2e-6, scale_after_normalization=True)
+        relu = tf.nn.relu(bn, "relu")
+
+        if summarize:
+            _activation_summary(conv)
+            _activation_summary(relu)
+            _variable_summary(beta)
+            _variable_summary(gamma)
+
+    return relu
+
+
 def max_pool_2x2(t):
     '''
     Run a 2x2 max pooling operation
@@ -69,7 +124,7 @@ def conv_bn_relu(input_tensor, kernel_size, out_filters, scope,
     # gamma is the scalar value to be multiplied after batch norm
     gamma_init = tf.constant_initializer(1.0)
 
-    with tf.variable_scope(scope, reuse=True) as scope:
+    with tf.variable_scope(scope) as scope:
         conv_kernel = tf.get_variable(
             "conv_kernel", var_shape, initializer=kernel_init)
         conv = tf.nn.conv2d(input_tensor, conv_kernel, [1, 1, 1, 1],
